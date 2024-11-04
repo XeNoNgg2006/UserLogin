1bf1d73a2dbbc61152566e2688ccde1c3ff0970f
xenon <constantinbarba2006@gmail.com>
2024-11-04 21:49:54 +0100

    register(post), new libraries

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 const express = require('express')
 const app = express()
+const bcrypt = require("bcrypt")
+
+const users = []
+
+
+app.set("view-engine","ejs")
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {name: 'Kyle'})
 })
 
+app.get("/login", (req,res) => {
+    res.render('login.ejs')
+})
 
+app.post("/login", (req, res) => {
 
+})
+
+app.get("/register", (req,res) => {
+    res.render('register.ejs')
+})
+
+app.post("/register", async (req, res) =>{
+    try { // try and catch=> 
+                //try= tries a piece of code which might throw an error 
+                     //(ex. here= no name(only worked if the name wasnt required by form)
+        const hashedPassword = await bcrypt.hash(req.body.password, 10) // takes the inputed password and encrypts it
+        users.push( {
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect("/login")
+    } catch { // catch = what to do if the code throws an error
+        res.redirect("/register")
+    }
+    console.log(users)
+})
 
 
 app.listen(3000)
